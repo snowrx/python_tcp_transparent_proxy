7a14adc0c2be1fab1881b97b995108b75bc3f57d
snowrx <sal.snow3510+github@gmail.com>
2024-07-22 05:37:20 +0900

    limit: int = 1 << 20

--- proxy.py
+++ proxy.py
@@ -10,8 +10,7 @@ import struct
 class config:
     port: int = 8081
     timeout: int = 3660
-    limit: int = 1 << 18
-    chunk: int = 1 << 14
+    limit: int = 1 << 20
 
 
 class consts:
@@ -38,7 +37,7 @@ async def proxy(r: asyncio.StreamReader, w: asyncio.StreamWriter):
     try:
         s: socket.socket = w.get_extra_info("socket")
         s.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, True)
-        while not w.is_closing() and (data := await asyncio.wait_for(r.read(config.chunk), config.timeout)):
+        while not w.is_closing() and (data := await asyncio.wait_for(r.read(config.limit), config.timeout)):
             w.write(data)
             del data
             await w.drain()
