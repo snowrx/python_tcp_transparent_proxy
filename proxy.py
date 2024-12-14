cda9d6c1dda17be3900915c1a8e1a6e3ba1cf8fd
snowrx <sal.snow3510+github@gmail.com>
2024-12-14 17:00:56 +0900

    modify logging 2

--- proxy.py
+++ proxy.py
@@ -63,14 +63,14 @@ class Listener:
             logging.warning(f"[{self._pid}:{cid}] Failed proxy {src[0]}@{src[1]} <> {dst[0]}@{dst[1]}")
             return
 
-        logging.info(f"[{self._pid}:{cid}] Open proxy {src[0]}@{src[1]} <> {dst[0]}@{dst[1]} ({round(open_delay * 1000)}ms)")
+        logging.info(f"[{self._pid}:{cid}] Established proxy {src[0]}@{src[1]} <> {dst[0]}@{dst[1]} ({round(open_delay * 1000)}ms)")
         lc = Connector(self._pid, cid, 0, cr, pw)
         pc = Connector(self._pid, cid, 1, pr, cw)
         proxy_start = time.perf_counter()
         async with asyncio.TaskGroup() as tg:
             _ = (tg.create_task(lc.proxy()), tg.create_task(pc.proxy()))
         proxy_duration = time.perf_counter() - proxy_start
-        logging.info(f"[{self._pid}:{cid}] Close proxy {src[0]}@{src[1]} <> {dst[0]}@{dst[1]} {round(proxy_duration)}s")
+        logging.info(f"[{self._pid}:{cid}] Closed proxy {src[0]}@{src[1]} <> {dst[0]}@{dst[1]} {round(proxy_duration)}s")
 
     def _get_original_dst(self, so: socket.socket, is_ipv4=True):
         if is_ipv4:
