b524bd0f335d834207a092bb37604b9d9659ebb5
snowrx <sal.snow3510+github@gmail.com>
2024-12-31 21:06:06 +0900

    1MiB LIMIT

--- proxy.py
+++ proxy.py
@@ -1,7 +1,5 @@
-from concurrent.futures import ProcessPoolExecutor
 import asyncio
 import logging
-import os
 import socket
 import struct
 import time
@@ -9,7 +7,7 @@ import time
 PORT = 8081
 LIFETIME = 86400
 CHUNK_SIZE = 2**14
-ENABLE_MP = False
+LIMIT = 2**20
 
 
 class Listener:
@@ -18,20 +16,18 @@ class Listener:
     _V4_LEN = 16
     _V6_LEN = 28
 
-    _pid = 0
     _live = 0
 
-    def run(self, pid=0):
+    def run(self):
         async def _server():
-            server = await asyncio.start_server(self._client, port=PORT, reuse_port=True)
+            server = await asyncio.start_server(self._client, port=PORT, limit=LIMIT)
             async with server:
                 await server.serve_forever()
             for t in asyncio.all_tasks():
                 t.cancel()
-                logging.debug(f"[{self._pid}] Cancelled {t.get_name()}")
+                logging.debug(f"Cancelled {t.get_name()}")
 
-        self._pid = pid
-        logging.info(f"[{self._pid}] Listening {PORT=}")
+        logging.info(f"Listening {PORT=}")
         asyncio.run(_server())
 
     async def _client(self, cr: asyncio.StreamReader, cw: asyncio.StreamWriter):
@@ -44,22 +40,22 @@ class Listener:
         r_label = f"{dst[0]}@{dst[1]} > {src[0]}@{src[1]}"
 
         if dst[0] == srv[0] and dst[1] == srv[1]:
-            logging.warning(f"[{self._pid}] Blocked {w_label}")
+            logging.warning(f"Blocked {w_label}")
             await writer_close(cw)
             return
 
         try:
-            pr, pw = await asyncio.open_connection(host=dst[0], port=dst[1])
+            pr, pw = await asyncio.open_connection(host=dst[0], port=dst[1], limit=LIMIT)
         except:
-            logging.warning(f"[{self._pid}] Connection failed {w_label}")
+            logging.warning(f"Connection failed {w_label}")
             await writer_close(cw)
             return
 
         self._live += 1
-        writer = Channel(self._pid, w_label, cr, pw)
-        reader = Channel(self._pid, r_label, pr, cw)
-        logging.info(f"[{self._pid}] Established {w_label}")
-        logging.debug(f"[{self._pid}] live={self._live}")
+        writer = Channel(w_label, cr, pw)
+        reader = Channel(r_label, pr, cw)
+        logging.info(f"Established {w_label}")
+        logging.debug(f"live={self._live}")
 
         proxy_start = time.perf_counter()
         async with asyncio.TaskGroup() as tg:
@@ -68,8 +64,8 @@ class Listener:
         await writer_close(cw)
         proxy_time = round(time.perf_counter() - proxy_start)
         self._live -= 1
-        logging.info(f"[{self._pid}] Closed {proxy_time}s {w_label}")
-        logging.debug(f"[{self._pid}] live={self._live}")
+        logging.info(f"Closed {proxy_time}s {w_label}")
+        logging.debug(f"live={self._live}")
 
     def _get_original_dst(self, so: socket.socket, is_ipv4=True):
         if is_ipv4:
@@ -84,13 +80,11 @@ class Listener:
 
 
 class Channel:
-    _pid: int
     _label: str
     _r: asyncio.StreamReader
     _w: asyncio.StreamWriter
 
-    def __init__(self, pid: int, label: str, r: asyncio.StreamReader, w: asyncio.StreamWriter):
-        self._pid = pid
+    def __init__(self, label: str, r: asyncio.StreamReader, w: asyncio.StreamWriter):
         self._label = label
         self._r = r
         self._w = w
@@ -101,17 +95,21 @@ class Channel:
             s.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, True)
 
             async with asyncio.timeout(LIFETIME):
-                while data := await self._r.read(CHUNK_SIZE):
+                while data := await self._r.read(LIMIT):
+                    t = time.perf_counter_ns()
                     self._w.write(data)
                     await self._w.drain()
+                    write_time = round((time.perf_counter_ns() - t) / 1000000)
+                    if write_time > 100:
+                        logging.warning(f"Slow write {write_time=}ms {self._label}")
 
                 self._r.feed_eof()
                 self._w.write_eof()
                 await self._w.drain()
-                logging.debug(f"[{self._pid}] EOF {self._label}")
+                logging.debug(f"EOF {self._label}")
 
         except Exception as err:
-            logging.debug(f"[{self._pid}] Error {err} {self._label}")
+            logging.debug(f"Error {err} {self._label}")
 
 
 async def writer_close(writer: asyncio.StreamWriter):
@@ -125,12 +123,7 @@ async def writer_close(writer: asyncio.StreamWriter):
 
 def main():
     logging.basicConfig(level=logging.DEBUG)
-    cpu_count = os.cpu_count() or 1
-    if ENABLE_MP and cpu_count > 1:
-        with ProcessPoolExecutor(cpu_count) as executor:
-            executor.map(Listener().run, range(cpu_count))
-    else:
-        Listener().run()
+    Listener().run()
 
 
 if __name__ == "__main__":
