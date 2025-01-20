04f358eb9cac2af46bd7acb73c5da9aff3041935
snowrx <sal.snow3510+github@gmail.com>
2025-01-21 05:33:00 +0900

    delete preload, decrease limit

--- proxy.py
+++ proxy.py
@@ -7,11 +7,10 @@ import time
 
 PORT = 8081
 LIFETIME = 14400
-PRELOAD = 2**23
 
 
 class proxy:
-    _DEFAULT_LIMIT = 2**16
+    _LIMIT = 2**14
     _SO_ORIGINAL_DST = 80
     _SOL_IPV6 = 41
     _V4_LEN = 16
@@ -41,11 +40,11 @@ class proxy:
         try:
             s: socket.socket = w.get_extra_info("socket")
             s.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, True)
-            w.transport.set_write_buffer_limits(PRELOAD, PRELOAD)
+            w.transport.set_write_buffer_limits(self._LIMIT, self._LIMIT)
 
             status = "read"
             async with asyncio.timeout(LIFETIME):
-                while data := await r.read(self._DEFAULT_LIMIT):
+                while data := await r.read(self._LIMIT):
                     status = "write"
                     w.write(data)
                     await w.drain()
