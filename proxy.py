a8ba14144fc5e46f57e25d792db804315ded7b31
snowrx <sal.snow3510+github@gmail.com>
2025-02-11 15:45:31 +0900

    run in main thread

--- proxy.py
+++ proxy.py
@@ -1,4 +1,3 @@
-from concurrent.futures import ThreadPoolExecutor
 import asyncio
 import gc
 import logging
@@ -123,5 +122,4 @@ if __name__ == "__main__":
     gc.set_threshold(10000)
     gc.set_debug(gc.DEBUG_STATS)
     logging.basicConfig(level=logging.DEBUG)
-    with ThreadPoolExecutor() as tp:
-        tp.submit(proxy().run)
+    proxy().run()
