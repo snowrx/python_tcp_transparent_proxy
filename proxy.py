dbfcea57e7042f9236a499a2061f5145b8ffa369
snowrx <sal.snow3510+github@gmail.com>
2025-02-17 14:47:20 +0900

    connection managing by read timeout, change log format

--- proxy.py
+++ proxy.py
@@ -1,4 +1,3 @@
-from dataclasses import dataclass, field
 import asyncio
 import gc
 import logging
@@ -7,13 +6,7 @@ import struct
 import time
 
 PORT = 8081
-LIFETIME = 86400
-
-
-@dataclass(order=True)
-class ticket:
-    ts: int = field(default_factory=time.monotonic_ns, compare=True)
-    ev: asyncio.Event = field(default_factory=asyncio.Event, compare=False)
+READ_TIMEOUT = 3600
 
 
 class proxy:
@@ -22,8 +15,6 @@ class proxy:
     _SOL_IPV6 = 41
     _V4_LEN = 16
     _V6_LEN = 28
-
-    _wq: asyncio.PriorityQueue[ticket] = asyncio.PriorityQueue()
 
     def get_original_dst(self, so: socket.socket, is_ipv4: bool = True):
         if is_ipv4:
@@ -36,44 +27,44 @@ class proxy:
             ip = socket.inet_ntop(socket.AF_INET6, raw_ip)
         return ip, port
 
-    async def writer_close(self, w: asyncio.StreamWriter):
+    async def writer_close(self, w: asyncio.StreamWriter, abort: bool = False):
         try:
             if not w.is_closing():
+                if abort:
+                    w.transport.abort()
                 w.close()
                 await w.wait_closed()
         except Exception as err:
-            logging.error(f"Failed to close writer: {err}")
+            logging.error(f"Failed to writer_close: {type(err).__name__}")
+
+    async def read_data(self, r: asyncio.StreamReader):
+        async with asyncio.timeout(READ_TIMEOUT):
+            return await r.read(self._READ_CHUNK_SIZE)
 
     async def proxy(self, label: str, r: asyncio.StreamReader, w: asyncio.StreamWriter):
         try:
-            read = asyncio.create_task(r.read(self._READ_CHUNK_SIZE))
+            read = asyncio.create_task(self.read_data(r))
             s: socket.socket = w.get_extra_info("socket")
             s.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, True)
-            w.transport.set_write_buffer_limits(self._READ_CHUNK_SIZE, self._READ_CHUNK_SIZE)
+            w.transport.set_write_buffer_limits(self._READ_CHUNK_SIZE)
 
-            async with asyncio.timeout(LIFETIME):
-                t = ticket()
-                data = await read
-                while not w.is_closing() and data:
-                    read = asyncio.create_task(r.read(self._READ_CHUNK_SIZE))
-                    await self._wq.put(t)
-                    await t.ev.wait()
-                    t.ev.clear()
-                    w.write(data)
-                    await w.drain()
-                    data = await read
+            await asyncio.sleep(0)
+            logging.debug(f"Start channel: {label}")
+            while not w.is_closing() and (data := await read):
+                read = asyncio.create_task(self.read_data(r))
+                w.write(data)
+                await w.drain()
 
             if not w.is_closing():
                 w.write_eof()
                 await w.drain()
 
             r.feed_eof()
-            logging.debug(f"EOF {label}")
+            logging.debug(f"Finished channel: {label}")
 
         except Exception as err:
-            logging.error(f"{label}: {err}")
-            w.transport.abort()
-            await self.writer_close(w)
+            logging.error(f"Error channel: {type(err).__name__}, {label}")
+            await self.writer_close(w, True)
 
     async def client(self, cr: asyncio.StreamReader, cw: asyncio.StreamWriter):
         soc: socket.socket = cw.get_extra_info("socket")
@@ -84,27 +75,26 @@ class proxy:
         try:
             dst: tuple[str, int] = await asyncio.to_thread(self.get_original_dst, soc, is_ipv4)
         except Exception as err:
-            logging.error(f"Failed to get original destination: {err}")
-            await self.writer_close(cw)
+            logging.error(f"Failed to get_original_dst: {type(err).__name__}, {src[0]}@{src[1]}")
+            await self.writer_close(cw, True)
             return
+
         w_label = f"{src[0]}@{src[1]} -> {dst[0]}@{dst[1]}"
         r_label = f"{src[0]}@{src[1]} <- {dst[0]}@{dst[1]}"
 
         if dst[0] == srv[0] and dst[1] == srv[1]:
-            logging.error(f"Blocked direct connection {w_label}")
-            cw.transport.abort()
-            await self.writer_close(cw)
+            logging.error(f"Refused to connect: {w_label}")
+            await self.writer_close(cw, True)
             return
 
         try:
             pr, pw = await asyncio.open_connection(host=dst[0], port=dst[1])
         except Exception as err:
-            logging.error(f"Failed to open connection {w_label}: {err}")
-            cw.transport.abort()
-            await self.writer_close(cw)
+            logging.error(f"Failed to open_connection: {type(err).__name__}, {w_label}")
+            await self.writer_close(cw, True)
             return
 
-        logging.info(f"Connection established {w_label}")
+        logging.info(f"Established: {w_label}")
         proxy_start = time.monotonic()
         async with asyncio.TaskGroup() as tg:
             tg.create_task(self.proxy(r_label, pr, cw))
@@ -112,29 +102,22 @@ class proxy:
         await self.writer_close(pw)
         await self.writer_close(cw)
         proxy_time = round(time.monotonic() - proxy_start)
-        logging.info(f"Connection closed {w_label} in {proxy_time} seconds")
+        logging.info(f"Closed: {w_label}, {proxy_time}s")
 
     async def server(self):
         server = await asyncio.start_server(self.client, port=PORT)
         async with server:
+            for sock in server.sockets:
+                logging.info(f"Listening on {sock.getsockname()}")
             await server.serve_forever()
         for t in asyncio.all_tasks():
             t.cancel()
 
-    async def consumer(self):
-        while True:
-            t = await self._wq.get()
-            t.ts = time.monotonic_ns()
-            t.ev.set()
-            self._wq.task_done()
-
     async def launch(self):
         async with asyncio.TaskGroup() as tg:
             tg.create_task(self.server())
-            tg.create_task(self.consumer())
 
     def run(self):
-        logging.info(f"Proxy server started on port {PORT}")
         asyncio.run(self.launch())
 
 
