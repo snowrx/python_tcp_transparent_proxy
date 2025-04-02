2d561b32e0f2c0787a881e885eb255139aafa752
snowrx <sal.snow3510+github@gmail.com>
2025-04-02 11:02:37 +0900

    proxy: adjust write buffer limits to match LOOKAHEAD size

--- proxy.py
+++ proxy.py
@@ -44,7 +44,7 @@ class proxy:
         try:
             s: socket.socket = w.get_extra_info("socket")
             s.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, True)
-            w.transport.set_write_buffer_limits(LOOKAHEAD, 0)
+            w.transport.set_write_buffer_limits(LOOKAHEAD, LOOKAHEAD)
             await asyncio.sleep(0)
 
             async with asyncio.timeout(CONNECTION_LIFETIME):
