1c7fe27c22af6456b930a92d801052e171b95db8
snowrx <sal.snow3510+github@gmail.com>
2025-06-22 22:18:45 +0900

    use uvloop and optimize logics

--- proxy.py
+++ proxy.py
@@ -1,15 +1,14 @@
-from concurrent.futures import ThreadPoolExecutor
 import asyncio
 import gc
 import logging
-import os
 import socket
 import struct
+
+import uvloop
 
 PORT = 8081
 LIFETIME = 86400
 LIMIT = 1 << 18
-NCORES = 4
 
 
 class channel:
@@ -23,13 +22,12 @@ class channel:
         self._label = label
 
     async def streaming(self):
-        read_task = asyncio.create_task(self._reader.read(LIMIT))
-        while v := memoryview(await read_task):
-            self._writer.write(v)
-            read_task = asyncio.create_task(self._reader.read(LIMIT))
+        while not self._writer.is_closing() and (b := await self._reader.read(LIMIT)):
             await self._writer.drain()
-        self._writer.write_eof()
-        await self._writer.drain()
+            self._writer.write(b)
+        if not self._writer.is_closing():
+            self._writer.write_eof()
+            await self._writer.drain()
 
     async def transfer(self):
         try:
@@ -38,10 +36,9 @@ class channel:
             so.setsockopt(socket.SOL_TCP, socket.TCP_NODELAY, True)
             self._writer.transport.set_write_buffer_limits(LIMIT, LIMIT)
             async with asyncio.timeout(LIFETIME):
-                await asyncio.create_task(self.streaming())
+                await self.streaming()
         except Exception as err:
             logging.error(f"Error in channel: {self._label}: {err}")
-            self._writer.transport.abort()
         finally:
             if not self._writer.is_closing():
                 try:
@@ -57,37 +54,50 @@ class server:
     _V4_LEN = 16
     _V6_LEN = 28
 
-    def get_original_dst(self, so: socket.socket, is_ipv4: bool = True):
-        if is_ipv4:
-            dst = so.getsockopt(socket.SOL_IP, self._SO_ORIGINAL_DST, self._V4_LEN)
-            port, raw_ip = struct.unpack_from("!2xH4s", dst)
-            ip = socket.inet_ntop(socket.AF_INET, raw_ip)
-        else:
-            dst = so.getsockopt(self._SOL_IPV6, self._SO_ORIGINAL_DST, self._V6_LEN)
-            port, raw_ip = struct.unpack_from("!2xH4x16s", dst)
-            ip = socket.inet_ntop(socket.AF_INET6, raw_ip)
+    def get_original_dst(self, so: socket.socket):
+        ip: str
+        port: int
+        match so.family:
+            case socket.AF_INET:
+                dst = so.getsockopt(socket.SOL_IP, self._SO_ORIGINAL_DST, self._V4_LEN)
+                port, raw_ip = struct.unpack_from("!2xH4s", dst)
+                ip = socket.inet_ntop(socket.AF_INET, raw_ip)
+            case socket.AF_INET6:
+                dst = so.getsockopt(self._SOL_IPV6, self._SO_ORIGINAL_DST, self._V6_LEN)
+                port, raw_ip = struct.unpack_from("!2xH4x16s", dst)
+                ip = socket.inet_ntop(socket.AF_INET6, raw_ip)
+            case _:
+                raise ValueError(f"Unsupported address family: {so.family}")
         return ip, port
 
     async def accept(self, client_reader: asyncio.StreamReader, client_writer: asyncio.StreamWriter):
-        so: socket.socket = client_writer.get_extra_info("socket")
+        try:
+            orig: tuple[str, int] = self.get_original_dst(client_writer.get_extra_info("socket"))
+        except Exception as err:
+            logging.error(f"Failed to get original destination: {err}")
+            client_writer.close()
+            await client_writer.wait_closed()
+            return
+
+        t_open = asyncio.create_task(asyncio.open_connection(orig[0], orig[1], limit=LIMIT))
+        await asyncio.sleep(0)
         peername: tuple[str, int] = client_writer.get_extra_info("peername")
         sockname: tuple[str, int] = client_writer.get_extra_info("sockname")
-        orig: tuple[str, int] = await asyncio.to_thread(self.get_original_dst, so, "." in peername[0])
         read_label = f"{orig[0]}@{orig[1]} -> {peername[0]}@{peername[1]}"
         write_label = f"{peername[0]}@{peername[1]} -> {orig[0]}@{orig[1]}"
 
         if orig[0] == sockname[0] and orig[1] == sockname[1]:
             logging.error(f"Blocked loopback connection: {write_label}")
-            client_writer.transport.abort()
+            t_open.cancel()
             client_writer.close()
             await client_writer.wait_closed()
             return
 
         try:
-            orig_reader, orig_writer = await asyncio.open_connection(orig[0], orig[1], limit=LIMIT)
+            orig_reader, orig_writer = await t_open
         except Exception as err:
             logging.error(f"Failed to connect: {write_label}: {err}")
-            client_writer.transport.abort()
+            t_open.cancel()
             client_writer.close()
             await client_writer.wait_closed()
             return
@@ -101,28 +111,16 @@ class server:
         logging.info(f"Closed connection: {write_label}")
 
     async def start_server(self):
-        loop = asyncio.get_running_loop()
-        loop.set_task_factory(asyncio.eager_task_factory)
-        loop.set_default_executor(ThreadPoolExecutor(1))
+        asyncio.get_event_loop().set_task_factory(asyncio.eager_task_factory)
+        self._queue = asyncio.Queue()
         server = await asyncio.start_server(self.accept, port=PORT, limit=LIMIT, reuse_port=True)
         logging.info(f"Listening on port {PORT}")
         async with server:
-            await asyncio.create_task(server.serve_forever())
-
-
-def run(_=None):
-    asyncio.run(server().start_server())
+            await server.serve_forever()
 
 
 if __name__ == "__main__":
     gc.collect()
-    gc.freeze()
-    gc.set_threshold(3000)
     gc.set_debug(gc.DEBUG_STATS)
     logging.basicConfig(level=logging.DEBUG)
-
-    if NCORES > 1:
-        with ThreadPoolExecutor(NCORES) as executor:
-            executor.map(run, range(NCORES))
-    else:
-        run()
+    uvloop.run(server().start_server())
