8328a404ff05e84293687b3c1d1d6cbbd0c59f22
snowrx <sal.snow3510+github@gmail.com>
2025-06-28 01:53:44 +0900

    uvloop

--- proxy.py
+++ proxy.py
@@ -1,10 +1,10 @@
-from concurrent.futures import ThreadPoolExecutor
 import asyncio
 import gc
 import logging
-import os
 import socket
 import struct
+
+import uvloop
 
 PORT = 8081
 LIFETIME = 86400
@@ -12,8 +12,7 @@ LIMIT = 1 << 18
 
 
 class channel:
-    def __init__(self, pid: int, reader: asyncio.StreamReader, writer: asyncio.StreamWriter, label: str):
-        self._pid: int = pid
+    def __init__(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter, label: str):
         self._reader: asyncio.StreamReader = reader
         self._writer: asyncio.StreamWriter = writer
         self._label: str = label
@@ -34,14 +33,14 @@ class channel:
             async with asyncio.timeout(LIFETIME):
                 await self.streaming()
         except Exception as err:
-            logging.error(f"[{self._pid}] Error in channel: {self._label}, {err}")
+            logging.error(f"Error in channel: {self._label}, {err}")
         finally:
             if not self._writer.is_closing():
                 try:
                     self._writer.close()
                     await self._writer.wait_closed()
                 except Exception as err:
-                    logging.debug(f"[{self._pid}] Failed to close writer: {self._label}, {err}")
+                    logging.debug(f"Failed to close writer: {self._label}, {err}")
 
 
 class server:
@@ -49,11 +48,6 @@ class server:
     _SOL_IPV6 = 41
     _V4_LEN = 16
     _V6_LEN = 28
-
-    def __init__(self, pid: int):
-        self._pid: int = pid
-        self._fid: int = 0
-        gc.set_debug(gc.DEBUG_STATS)
 
     def get_original_dst(self, so: socket.socket):
         ip: str
@@ -75,7 +69,8 @@ class server:
         try:
             orig: tuple[str, int] = self.get_original_dst(client_writer.get_extra_info("socket"))
         except Exception as err:
-            logging.error(f"[{self._pid}] Failed to get original destination: {err}")
+            logging.error(f"Failed to get original destination: {err}")
+            client_writer.transport.abort()
             client_writer.close()
             await client_writer.wait_closed()
             return
@@ -86,41 +81,39 @@ class server:
         write_label = f"{peername[0]}@{peername[1]} -> {orig[0]}@{orig[1]}"
 
         if orig[0] == sockname[0] and orig[1] == sockname[1]:
-            logging.error(f"[{self._pid}] Blocked loopback connection: {write_label}")
+            logging.error(f"Blocked loopback connection: {write_label}")
+            client_writer.transport.abort()
             client_writer.close()
             await client_writer.wait_closed()
             return
 
         try:
-            logging.debug(f"[{self._pid}] calling: {write_label}")
+            logging.debug(f"calling: {write_label}")
             orig_reader, orig_writer = await asyncio.open_connection(orig[0], orig[1], limit=LIMIT)
         except Exception as err:
-            logging.error(f"[{self._pid}] Failed to connect: {write_label}, {err}")
+            logging.error(f"Failed to connect: {write_label}, {err}")
+            client_writer.transport.abort()
             client_writer.close()
             await client_writer.wait_closed()
             return
 
-        logging.info(f"[{self._pid}] Established connection: {write_label}")
-        read_channel = channel(self._pid, orig_reader, client_writer, read_label)
-        write_channel = channel(self._pid, client_reader, orig_writer, write_label)
+        logging.info(f"Established connection: {write_label}")
+        read_channel = channel(orig_reader, client_writer, read_label)
+        write_channel = channel(client_reader, orig_writer, write_label)
         async with asyncio.TaskGroup() as tg:
             tg.create_task(read_channel.transfer())
             tg.create_task(write_channel.transfer())
-        logging.info(f"[{self._pid}] Closed connection: {write_label}")
+        logging.info(f"Closed connection: {write_label}")
 
     async def start_server(self):
-        loop = asyncio.get_running_loop()
-        loop.set_task_factory(asyncio.eager_task_factory)
-        loop.set_default_executor(ThreadPoolExecutor(1))
-        server = await asyncio.start_server(self.accept, port=PORT, limit=LIMIT, reuse_port=True)
-        logging.info(f"[{self._pid}] Listening on port {PORT}")
+        server = await asyncio.start_server(self.accept, port=PORT, limit=LIMIT)
+        logging.info(f"Listening on port {PORT}")
         async with server:
             await server.serve_forever()
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
-    worker_count = len(os.sched_getaffinity(0))
-    with ThreadPoolExecutor(worker_count) as executor:
-        executor.map(asyncio.run, [server(i).start_server() for i in range(worker_count)])
+    gc.set_debug(gc.DEBUG_STATS)
+    uvloop.run(server().start_server())
     logging.shutdown()
