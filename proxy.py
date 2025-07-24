52d789b21c0924068c64e5d55405b092e0e7b65d
snowrx <sal.snow3510@gmail.com>
2025-07-24 12:27:30 +0900

    Revert "eager"
    This reverts commit eb0e27e17c464ef560e7a79c64c855a3628dab54.

--- proxy.py
+++ proxy.py
@@ -9,7 +9,6 @@ LOG = logging.DEBUG
 PORT = 8081
 LIFETIME = 86400
 LIMIT = 1 << 16
-EAGERNESS = 2
 
 
 class util:
@@ -89,7 +88,7 @@ class server:
             return
 
         try:
-            pr, pw = await asyncio.open_connection(*dst, limit=LIMIT << EAGERNESS)
+            pr, pw = await asyncio.open_connection(*dst, limit=LIMIT)
         except Exception as err:
             logging.error(f"Failed to open connection: {label}, {err}")
             await self.abort(cw)
@@ -108,8 +107,7 @@ class server:
             pass
 
     async def start_server(self):
-        asyncio.get_running_loop().set_task_factory(asyncio.eager_task_factory)
-        server = await asyncio.start_server(self.accept, port=PORT, limit=LIMIT << EAGERNESS)
+        server = await asyncio.start_server(self.accept, port=PORT, limit=LIMIT)
         logging.info(f"Listening on port {PORT}")
         async with server:
             await server.serve_forever()
