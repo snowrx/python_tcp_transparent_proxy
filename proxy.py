2a271d54a9c7ab042c986822a7470a5e74ce147e
snowrx <sal.snow3510+github@gmail.com>
2025-08-06 00:39:17 +0900

    add label

--- proxy.py
+++ proxy.py
@@ -33,10 +33,9 @@ class util:
 
 
 class proxy:
-    async def streaming(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
-        so: socket.socket = writer.get_extra_info("socket")
-        peer: tuple[str, int] = writer.get_extra_info("peername")
+    async def streaming(self, label: str, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
         try:
+            so: socket.socket = writer.get_extra_info("socket")
             so.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
             writer.transport.set_write_buffer_limits(LIMIT, LIMIT)
             async with asyncio.timeout(LIFETIME):
@@ -44,13 +43,13 @@ class proxy:
                     await writer.drain()
                     writer.write(data)
         except Exception as e:
-            logging.error(f"streaming: [{peer[0]}]:{peer[1]}: {type(e).__name__}: {e}")
+            logging.error(f"streaming {label}: {type(e).__name__}: {e}")
         finally:
             try:
                 writer.close()
                 await writer.wait_closed()
             except Exception as e:
-                logging.debug(f"close: [{peer[0]}]:{peer[1]}: {type(e).__name__}: {e}")
+                logging.error(f"close {label}: {type(e).__name__}: {e}")
 
     async def accept(self, client_reader: asyncio.StreamReader, client_writer: asyncio.StreamWriter):
         try:
@@ -64,19 +63,21 @@ class proxy:
             await client_writer.wait_closed()
             return
 
+        writer = f"[{peer[0]}]:{peer[1]} -> [{orig[0]}]:{orig[1]}"
+        reader = f"[{peer[0]}]:{peer[1]} <- [{orig[0]}]:{orig[1]}"
         try:
             proxy_reader, proxy_writer = await asyncio.open_connection(*orig, limit=LIMIT)
         except Exception as e:
-            logging.error(f"open_connection: [{orig[0]}]:{orig[1]}: {type(e).__name__}: {e}")
+            logging.error(f"open_connection: {writer}: {type(e).__name__}: {e}")
             client_writer.close()
             await client_writer.wait_closed()
             return
 
-        logging.info(f"open [{peer[0]}]:{peer[1]} <-> [{orig[0]}]:{orig[1]}")
+        logging.info(f"open: {writer}")
         async with asyncio.TaskGroup() as tg:
-            tg.create_task(self.streaming(client_reader, proxy_writer))
-            tg.create_task(self.streaming(proxy_reader, client_writer))
-        logging.info(f"close [{peer[0]}]:{peer[1]} <-> [{orig[0]}]:{orig[1]}")
+            tg.create_task(self.streaming(writer, client_reader, proxy_writer))
+            tg.create_task(self.streaming(reader, proxy_reader, client_writer))
+        logging.info(f"close: {writer}")
 
     async def run(self):
         asyncio.get_running_loop().set_task_factory(asyncio.eager_task_factory)
