30c00c0959e1b505b9472286991bc72871abc71b
snowrx <sal.snow3510+github@gmail.com>
2025-11-04 18:03:10 +0900

    64K

--- proxy.py
+++ proxy.py
@@ -9,7 +9,7 @@ import uvloop
 LOG_LEVEL = logging.DEBUG
 PORT = 8081
 CONN_LIFE = 86400
-LIMIT = 1 << 20
+CHUNK_SIZE = 1 << 16
 
 
 class Server:
@@ -43,7 +43,7 @@ class Server:
             sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
             sock.setsockopt(socket.SOL_TCP, socket.TCP_QUICKACK, 1)
             async with asyncio.timeout(CONN_LIFE):
-                while v := memoryview(await reader.read(LIMIT)):
+                while v := memoryview(await reader.read(CHUNK_SIZE)):
                     await writer.drain()
                     writer.write(v)
         except Exception as e:
@@ -66,7 +66,7 @@ class Server:
             return
 
         try:
-            proxy_reader, proxy_writer = await asyncio.open_connection(*dstname, limit=LIMIT)
+            proxy_reader, proxy_writer = await asyncio.open_connection(*dstname)
         except Exception as e:
             logging.error(f"Failed to connect {up_flow}: {type(e).__name__} {e}")
             client_writer.transport.abort()
@@ -85,7 +85,7 @@ class Server:
         logging.info(f"Closed {up_flow}")
 
     async def run(self):
-        server = await asyncio.start_server(self._accept, port=PORT, limit=LIMIT)
+        server = await asyncio.start_server(self._accept, port=PORT)
         logging.info(f"Listening on port {PORT}")
         async with server:
             await server.serve_forever()
@@ -98,6 +98,7 @@ async def main():
 
 
 if __name__ == "__main__":
+    gc.set_threshold(10000)
     gc.collect()
     gc.set_debug(gc.DEBUG_STATS)
     uvloop.run(main())
