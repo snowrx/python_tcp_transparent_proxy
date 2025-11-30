4db73e04511314a8abc7805000735c9de2efc770
snowrx <sal.snow3510+github@gmail.com>
2025-11-30 15:30:10 +0900

    Leave it to the execution environment

--- proxy.py
+++ proxy.py
@@ -81,10 +81,6 @@ class Session:
 
         logging.debug(f"{self._up_label} Session created")
 
-    def __del__(self):
-        logging.debug("Session destroyed")
-        del self._remote_sock, self._client_sock
-
     def _forward_data(self, label: str, src: socket.socket, dst: socket.socket):
         try:
             eof = False
