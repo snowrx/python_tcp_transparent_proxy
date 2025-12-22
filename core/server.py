91cb6c07fd0e563e1796877a5c99fd9842be7d1a
snowrx <sal.snow3510+github@gmail.com>
2025-12-22 23:59:38 +0900

    rewrite

--- core/server.py
+++ core/server.py
@@ -1,22 +1,21 @@
-from gevent import socket, monkey
+from gevent import socket
 from gevent.server import StreamServer
-
-monkey.patch_all()
 
 import logging
 
 
 class Server:
     def __init__(self, port: int, handler) -> None:
-        self._logger = logging.getLogger(f"{self.__class__.__name__}")
+        self._logger = logging.getLogger(f"{self.__class__.__name__}-{hex(id(self))}")
         self._port = port
 
-        sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-        sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 0)
-        sock.bind(("", port))
-        sock.listen(socket.SOMAXCONN)
-        self._server = StreamServer(sock, handler)
+        listener = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+        listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        listener.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 0)
+        listener.bind(("", self._port))
+        listener.listen(socket.SOMAXCONN)
+
+        self._server = StreamServer(listener, handler)
 
     def serve_forever(self) -> None:
         self._logger.info(f"Listening on port {self._port}")
--- main.py
+++ main.py
@@ -1,57 +1,45 @@
-import gevent
 from gevent import socket, monkey
-
-from core import server
 
 monkey.patch_all()
 
 import logging
 import os
-import gc
 
+from core.buffer_pool import BufferPool
 from core.server import Server
 from core.session import Session
-from core.buffer_pool import BufferPool
 from core.util import get_original_dst, ipv4_mapped
 
 LOG_LEVEL = logging.INFO
 LOG_FORMAT = "%(name)-30s | %(levelname)-10s | %(message)s"
 
 PORT = 8081
-IDLE_TIMEOUT = 7200
+TIMEOUT = 7200
 
-BUFFER_SIZE = 1 << 21
+BUFFER_SIZE = 1 << 20
 POOL_SIZE = 100
 
 
-def main():
+def main() -> None:
     buffer_pool = BufferPool(BUFFER_SIZE, POOL_SIZE)
 
     def handler(client_sock: socket.socket, client_addr: tuple[str, int]) -> None:
         with client_sock:
-            try:
-                family = socket.AF_INET if ipv4_mapped(client_addr[0]) else socket.AF_INET6
-                remote_addr = get_original_dst(client_sock, family)
-                srv_name = client_sock.getsockname()
-                if remote_addr[0] == srv_name[0] and remote_addr[1] == srv_name[1]:
-                    return
+            family = socket.AF_INET if ipv4_mapped(client_addr[0]) else socket.AF_INET6
+            remote_addr = get_original_dst(client_sock, family)
+            sockname = client_sock.getsockname()
+            if remote_addr == sockname:
+                return
 
-                with buffer_pool.buffer() as buffer:
-                    session = Session(client_sock, client_addr, remote_addr, family, buffer, IDLE_TIMEOUT)
-                    session.run()
+            with buffer_pool.acquire() as buffer:
+                Session(client_sock, client_addr, remote_addr, family, buffer, TIMEOUT).run()
 
-            except Exception as e:
-                logging.error(f"Session failed: {e}")
-
-    server = Server(PORT, handler)
-    server.serve_forever()
+    Server(PORT, handler).serve_forever()
 
 
 if __name__ == "__main__":
     if os.getenv("DEBUG"):
         LOG_LEVEL = logging.DEBUG
-        gc.set_debug(gc.DEBUG_STATS)
     logging.basicConfig(level=LOG_LEVEL, format=LOG_FORMAT)
-    gc.set_threshold(10000)
 
     main()
