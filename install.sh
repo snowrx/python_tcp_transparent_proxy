46e924e06e944d5a3d354ff078b5471eb66c8b4a
snowrx <sal.snow3510+github@gmail.com>
2025-12-23 21:29:46 +0900

    relogic

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 DEST=/opt/proxy
-DRY=0
 CLEAN=0
-EXEC=python3
+EXEC="$(which python3)"
 
 # check privileges
 if [ "$EUID" -ne 0 ]; then
@@ -13,36 +12,29 @@ fi
 
 while [ "$#" -gt 0 ]; do
   case $1 in
-    --dry)
-      DRY=1
-      echo "dry run"
-      ;;
     --clean)
       CLEAN=1
-      echo "clean=$CLEAN"
       ;;
     --exec)
       shift
       if [ "$1" != "" ]; then
-        EXEC=$1
-      fi
-      if [ "$EXEC" != "" ]; then
-        CLEAN=1
-        echo "exec=$EXEC"
+        EXEC="$1"
       fi
   esac
   shift
 done
 
-# check if proxy is already installed
+if [ $EXEC == "" ]; then
+  echo "Error: python executable not found"
+  exit 1
+fi
+
 if [ -f "/etc/systemd/system/proxy.service" ]; then
   systemctl stop proxy.service
   if [ $CLEAN -eq 1 ]; then
     echo "Cleaning up old installation"
-    if [ $DRY -ne 1 ]; then
-      rm -rf $DEST
-      mkdir -p $DEST
-    fi
+    rm -rf $DEST
+    mkdir -p $DEST
   fi
 else
   mkdir -p $DEST
@@ -56,6 +48,7 @@ touch proxy.env
 $EXEC -m venv .venv
 [ ! -f .venv/bin/activate ] && echo "venv not found" && exit 1
 source .venv/bin/activate
+pip install -U pip
 pip install -U -r requirements.txt
 deactivate
 
