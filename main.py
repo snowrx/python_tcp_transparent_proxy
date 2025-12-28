396ebadba210944c52f45bd4bfadc6e85a11686d
snowrx <sal.snow3510+github@gmail.com>
2025-12-29 01:22:39 +0900

    1

--- main.py
+++ main.py
@@ -15,10 +15,10 @@ LOG_FORMAT = "%(name)-30s | %(levelname)-10s | %(message)s"
 PORT = 8081
 TIMEOUT = 7200
 
-BUFFER_SIZE = 63 << 12
+BUFFER_SIZE = 1 << 20
 POOL_SIZE = 100
 
-WORKER_COUNT = 4
+WORKER_COUNT = 1
 
 
 def main() -> None:
